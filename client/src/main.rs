9b2acb3c19f1b1c0d651790b06a17758326b09bf
NaviHX <navihx@foxmail.com>
2025-11-27 11:06:12 +0800

    update(client): add cli parsing and ipc message construction

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+use common::{cli, ipc};
+use tracing::debug;
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt::init();
+    let args = cli::client::Args::parse();
+    let message = ipc::Message::from_cli_command(args.subcommand);
+
+    debug!("Message to be sent: {message:?}");
+
+    // TODO: Send the message to the daemon.
 }
