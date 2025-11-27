bbe60562014cd1644cadd457f06c86fabd810b80
NaviHX <navihx@foxmail.com>
2025-11-27 12:50:06 +0800

    update(client): impl sending and receiving

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,30 @@
+use anyhow::Result;
 use clap::Parser;
 use common::{cli, ipc};
-use tracing::debug;
+use std::os::unix::net::UnixStream;
+use tracing::{debug, error, info};
 
-fn main() {
+fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let args = cli::client::Args::parse();
     let message = ipc::Message::from_cli_command(args.subcommand);
 
     debug!("Message to be sent: {message:?}");
 
-    // TODO: Send the message to the daemon.
+    debug!("Trying to connect pwwwd socked ...");
+    let uds_path = ipc::default_uds_path()?;
+    let mut socket = UnixStream::connect(uds_path)?;
+
+    debug!("Trying to send the message to the daemon ...");
+    message.send(&mut socket)?;
+
+    debug!("Trying to receive reply from the daemon ...");
+    let reply = ipc::Reply::receive(&mut socket)?;
+
+    match reply {
+        ipc::Reply::Ok => info!("Ok"),
+        ipc::Reply::Error(e) => error!("Daemon encountered error when processing the request: {e}"),
+    }
+
+    Ok(())
 }
