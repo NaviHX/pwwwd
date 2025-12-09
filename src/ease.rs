adcab2d95978ff2321fbf8694e2744d80b4ba7f5
NaviHX <navihx@foxmail.com>
2025-12-09 12:35:25 +0800

    update(ease): impl cubic-bezier

--- src/ease.rs
+++ src/ease.rs
@@ -4,7 +4,7 @@ pub trait Curve {
     fn f(&self, x: f64) -> f64;
 }
 
-// pub mod cubic_bezier;
+pub mod cubic_bezier;
 pub mod static_curves;
 
 fn ease_with<C>(from: f64, to: f64, progress: f64, curve: impl Borrow<C>) -> f64
