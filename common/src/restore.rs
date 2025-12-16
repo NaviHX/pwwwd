7b187b1eb6c84e586fb26172465c67cda5c6abf6
NaviHX <navihx@foxmail.com>
2025-12-16 09:19:06 +0800

    fix(common/restore): add test for `Restore` serde

--- common/src/restore.rs
+++ common/src/restore.rs
@@ -36,3 +36,25 @@ impl Restore {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::path::Path;
+
+    #[test]
+    fn test_serde_restore() {
+        let restore = super::Restore::new(
+            Path::new("/home/test/test.jpg"),
+            crate::cli::server::ResizeOption::No,
+            (0, 0, 0),
+        );
+
+        let mut buf = vec![];
+        restore.serialize_to_buf(&mut buf).unwrap();
+        let new_restore = super::Restore::deserialize_from_buf(&buf).unwrap();
+
+        assert_eq!(restore.file_path, new_restore.file_path);
+        assert_eq!(restore.resize_option, new_restore.resize_option);
+        assert_eq!(restore.fill_rgb, new_restore.fill_rgb);
+    }
+}
